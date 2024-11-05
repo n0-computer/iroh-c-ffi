79931fb4173b050c2435631dc5ac9a8bf94f2805
“ramfox” <kasey@n0.computer>
2024-11-05 13:46:04 -0500

    see if the issue is related to the read timeout

--- main.c
+++ main.c
@@ -129,12 +129,16 @@ run_server (EndpointConfig_t * config, slice_ref_uint8_t alpn_slice, bool json_o
   } else {
     printf("receiving data\n");
   }
-  unsigned long bytes_read = 0;
-  int err = recv_stream_read_timeout(&recv_stream, recv_buffer_slice, &bytes_read, 10000);
-  if (err == ENDPOINT_RESULT_TIMEOUT) {
-    fprintf(stderr, "failed to read data before timeout");
-    return -1;
-    } else if (err == ENDPOINT_RESULT_READ_ERROR) {
+  // unsigned long bytes_read = 0;
+  int bytes_read = recv_stream_read(&recv_stream, recv_buffer_slice);
+  // if (err == ENDPOINT_RESULT_TIMEOUT) {
+  //   fprintf(stderr, "failed to read data before timeout");
+  //   return -1;
+  //   } else if (err == ENDPOINT_RESULT_READ_ERROR) {
+  //   fprintf(stderr, "failed to read data");
+  //   return -1;
+  // }
+  if (bytes_read == -1) {
     fprintf(stderr, "failed to read data");
     return -1;
   }
