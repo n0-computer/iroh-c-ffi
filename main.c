c38d3e681e9f95daca463c3bb51c05741ac5297f
“ramfox” <kasey@n0.computer>
2025-07-30 20:33:10 -0400

    see if we get a segfault if we dont free the `recv_str`

--- main.c
+++ main.c
@@ -463,14 +463,14 @@ run_client (
 
   printf("closing endpoint\n");
   endpoint_close(ep);
-  printf("~endpoint closed\n");
+  printf("endpoint closed\n");
 
   printf("fflush to stdout\n");
   fflush(stdout);
 
   // cleanup
-  printf("free recv_str\n");
-  free(recv_str);
+  // printf("free recv_str\n");
+  // free(recv_str);
   printf("free recv_buffer\n");
   free(recv_buffer);
   printf("free conn_status\n");
